5cc6919ca42cb9d7e6c186961ca74cd755ce17b7
Joeffison Andrade <joeffison@gmail.com>
2018-01-14 18:14:25 +0100

    Adding Logistic Regression (2nd Classification Model)

--- mlb_backend/mlbuilder/models.py
+++ mlb_backend/mlbuilder/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Create your models here.
+class MLModel(models.Model):
+    def __init__(self, name, model, pred_test, tar_test):
+        self.name = name
+        predictions = model.predict(pred_test)
+        # Analyze accuracy of predictions
+        self.confusion_matrix = confusion_matrix(tar_test, predictions)
+        self.accuracy_score = accuracy_score(tar_test, predictions)
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            #"confusion_matrix": self.confusion_matrix,
+            "accuracy_score": self.accuracy_score
+        }
--- mlb_backend/mlbuilder/views.py
+++ mlb_backend/mlbuilder/views.py
@@ -10,6 +10,8 @@ from django.core.files.storage import default_storage
 from django.http import HttpResponse, JsonResponse
 from rest_framework import status
 
+from .models import MLModel
+
 
 def __404_page():
     return HttpResponse("404 - Page not found ;)")
@@ -20,10 +22,8 @@ def index(request):
     return __404_page()
 
 def upload_csv(request):
-    data = {}
     if "GET" == request.method:
         return __404_page()
-    response_data = {"message": "Something went wrong."}
 
     # if not GET, then proceed
     try:
@@ -38,15 +38,9 @@ def upload_csv(request):
             messages.error(request, "Uploaded file is too big (%.2f MB)." % (csv_file.size / (1000 * 1000),))
             return HttpResponse(messages)
 
-        response_data = mlbuilder(csv_file)
-
+        return JsonResponse(mlbuilder(csv_file), status=status.HTTP_202_ACCEPTED, safe=False)
     except Exception as e:
-        #logging.getLogger("error_logger").error("Unable to upload file. " + repr(e))
-        messages.error(request, "Unable to upload file. " + repr(e))
         return HttpResponse(e.message + repr(e), status=status.HTTP_400_BAD_REQUEST)
-
-    return JsonResponse(response_data, status=status.HTTP_202_ACCEPTED, safe=False)
-
 
 def mlbuilder(csv_file):
     #TODO: It must have a better way, as the data is in <class 'django.core.files.uploadedfile.InMemoryUploadedFile'>.
@@ -55,7 +49,6 @@ def mlbuilder(csv_file):
     predictors = content.split("\n")[0].replace("\ufeff", "").replace("\r", "").split(",")
     targets = predictors[-1]
     predictors = predictors[:-1]
-    print predictors
     path = default_storage.save(os.path.join("tmp", csv_file.name), ContentFile(content))
     tmp_file = os.path.join(settings.MEDIA_ROOT, path)
 
@@ -64,28 +57,28 @@ def mlbuilder(csv_file):
 def __mlbuilder(csv_file, predictors, targets):
     import pandas as pd
     from sklearn.model_selection import train_test_split
-    from sklearn.metrics import classification_report
-    import sklearn.metrics
 
     historical_data = pd.read_csv(csv_file)
-    #, error_bad_lines=False, parse_dates=["transaction_date"], index_col="transaction_date"
-
     pred_train, pred_test, tar_train, tar_test = train_test_split(historical_data[predictors], historical_data[targets],
                                                                   test_size=.3)
-    decision_tree = __decision_tree(pred_train, tar_train)
-    predictions = decision_tree.predict(pred_test)
-    # Analyze accuracy of predictions
-    print sklearn.metrics.confusion_matrix(tar_test, predictions)
 
-    return [
-        {"name": "Decision Tree",
-         "accuracy_score": sklearn.metrics.accuracy_score(tar_test, predictions),
-         #"model": decision_tree
-        }
+    implemented_models = [
+        ("Decision Tree", __decision_tree),
+        ("Logistic Regression", __logistic_regression),
     ]
 
+    return {
+        #"Predictors_Correlation": historical_data.corr()[targets],
+        "models": [MLModel(model[0], model[1](pred_train, tar_train), pred_test, tar_test).to_json()
+                   for model in implemented_models]
+    }
+
 def __decision_tree(X, Y):
-    from sklearn import tree
-    model = tree.DecisionTreeClassifier()
-    model = model.fit(X, Y)
-    return model
+    from sklearn.tree import DecisionTreeClassifier
+    return DecisionTreeClassifier().fit(X, Y)
+
+# Despite its name, it's a classification algorithm
+# which predicts the probability of occurrence of an event by fitting data to a logit function.
+def __logistic_regression(X, Y):
+    from sklearn.linear_model import LogisticRegression
+    return LogisticRegression().fit(X, Y)
