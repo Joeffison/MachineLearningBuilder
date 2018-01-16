5169abd25ebd9ff299b6c3c1c6e0929cf127dce5
Joeffison Andrade <joeffison@gmail.com>
2018-01-16 18:52:18 +0100

    Adding API Auth Feature

--- mlb_backend/mlb_backend/settings.py
+++ mlb_backend/mlb_backend/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authentication.apps.AuthenticationConfig',
     'mlbuilder.apps.MlbuilderConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- mlb_backend/mlb_backend/urls.py
+++ mlb_backend/mlb_backend/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('authentication.urls')),
     url(r'^mlbuilder/', include('mlbuilder.urls')),
     url(r'^admin/', admin.site.urls),
 ]
